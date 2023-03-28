f48725b70f6722494700d0192a11bdeae24fb8e1
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-03-28 11:52:35 +0200

    Add products api route and model

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ import helmet from "helmet"
 import morgan from "morgan"
 import mongoose from "mongoose"
 import kpiRoutes from "./routes/kpi.js"
+import productRoutes from "./routes/product.js"
+import Product from "./models/Product.js"
 import KPI from "./models/KPI.js"
-import { kpis } from "./data/data.js"
+import { kpis, products } from "./data/data.js"
 
 // CONFIGURATIONS
 dotenv.config()
@@ -22,6 +24,7 @@ app.use(cors())
 
 // ROUTES
 app.use("/kpi", kpiRoutes)
+app.use("/product", productRoutes)
 
 // MONGOOSE CONFIGURATIONS
 const PORT = process.env.PORT || 9000;
@@ -31,8 +34,9 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
 
-    // ADD DATA ONE TIME ONLY
+    // ADD DATA ONE TIME ONLY OR AS NEEDED
     // await mongoose.connection.db.dropDatabase()
     // KPI.insertMany(kpis)
+    // Product.insertMany(products)
 
 }).catch((error) => console.log(`Error:${error}`))
